30e19d3ce965131926797e71da66447891e77b08
Lin Zhang <lin.zhang.ux@gmail.com>
2020-04-29 01:37:38 -0700

    add user facing templates for events

--- events/admin.py
+++ events/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+admin.site.site_header = "ThinkLocal Admin"
+admin.site.site_title = "ThinkLocal Admin"
+admin.site.index_title = "Welcome to ThinkLocal Admin Page"
+
 admin.site.register(Event)
 admin.site.register(Attendee)
--- events/views.py
+++ events/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Event
 # Create your views here.
+
+def index(request):
+    latest_event_list = Event.objects.order_by('-event_date')[:5]
+    context = {'latest_event_list': latest_event_list}
+    return render(request, 'events/index.html', context)
+
+# show specific event details
+
+def detail(request, event_id):
+    try:
+        event = Event.objects.get(pk = event_id)
+    except Event.DoesNotExist:
+        raise Http404('Event does not exist')
+    return render(request, 'events/detail.html', {'event':event})
