d2964fcb5909844d7eece12141272042c3f14e71
Lin Zhang <lin.zhang.ux@gmail.com>
2020-05-03 13:55:48 -0700

    add and setup mapping for page app - landing page

--- thinklocalapp/urls.py
+++ thinklocalapp/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pages.urls')),
     path('events/', include('events.urls')),
     path('admin/', admin.site.urls),
 ]
