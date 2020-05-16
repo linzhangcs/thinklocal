e1a00ae8493f92ea9c355d4270d9df1f8cf5d51e
Lin Zhang <lin.zhang.ux@gmail.com>
2020-05-15 18:47:07 -0700

    aws

--- thinklocalapp/settings.py
+++ thinklocalapp/settings.py
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'thinklocalapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'thinklocaldb',
-        # 'USER': 'thinklocaluser',
-        # 'PASSWORD': 'thinklocalapp',
-        # 'HOST': 'thinklocal.cfm2f60gx7yl.us-west-1.rds.amazonaws.com',
-        # 'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'thinklocaldb',
+        'USER': 'thinklocaluser',
+        'PASSWORD': 'thinklocalapp',
+        'HOST': 'thinklocal.cfm2f60gx7yl.us-west-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
